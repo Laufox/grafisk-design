6063c7e32c6d2ee65fd9b686d4051457a0f96c36
Tim Nilsson <tim.r.nilsson@gmail.com>
2021-12-15 22:50:11 +0100

    update

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const articleList = [
         id: 1,
         imgUrl: `img/thomas-a100.png`,
         heading: `“Pandemin gav mig tid att satsa digitalt”`,
-        text: `Annika Höglund som driver företaget HighGrove har haft digitalisering i tankarna länge. Men det var först när pandemin slog till – och det rejält i hennes resebransch – som hon själv slog till digitalt.`,
+        text: `Annika Höglund som driver företaget HighGrove har haft digitalisering i tankarna länge. Men det var först när pandemin slog till - och det rejält i hennes resebransch - som hon själv slog till digitalt.`,
         url: `https://www.almi.se/kunskapsbanken/inspiration/highgrove/`
     },
     {
@@ -138,4 +138,93 @@ btnNextArticlesEl.addEventListener('click', () => {
         `;
     }
     
+});
+
+let currentArtSlideLg = 0;
+const artOptLgEls = document.querySelectorAll('.opt-lg');
+const prevArtLg = document.querySelector('.prev-art-lg');
+const nextArtLg = document.querySelector('.next-art-lg');
+
+prevArtLg.addEventListener('click', () => {
+
+    if (currentArtSlideLg === 1) {
+        artOptLgEls[currentArtSlideLg].classList.remove('opt-selected');
+        currentArtSlideLg--;
+        artOptLgEls[currentArtSlideLg].classList.add('opt-selected');
+
+        document.querySelector('#experience .art-wrapper-lg').innerHTML = `
+        
+        <article>
+            <div class="d-flex justify-content-start align-items-center">
+                <div class="avatar-holder">
+                    <img src="${articleList[0].imgUrl}" alt="">
+                </div>
+                
+                <h3>${articleList[0].heading}</h3>
+            </div>
+            
+            <p>${articleList[0].text}</p>
+            
+            <a href="${articleList[0].url}">+ Läs hela artiklen</a>
+        </article>
+
+        <article>
+            <div class="d-flex justify-content-start align-items-center">
+                <div class="avatar-holder">
+                    <img src="${articleList[1].imgUrl}" alt="">
+                </div>
+                
+                <h3>${articleList[1].heading}</h3>
+            </div>
+            
+            <p>${articleList[1].text}</p>
+            
+            <a href="${articleList[1].url}">+ Läs hela artiklen</a>
+        </article>
+        
+        `;
+    }
+
+});
+
+nextArtLg.addEventListener('click', () => {
+
+    if (currentArtSlideLg === 0) {
+        artOptLgEls[currentArtSlideLg].classList.remove('opt-selected');
+        currentArtSlideLg++;
+        artOptLgEls[currentArtSlideLg].classList.add('opt-selected');
+
+        document.querySelector('#experience .art-wrapper-lg').innerHTML = `
+        
+        <article>
+            <div class="d-flex justify-content-start align-items-center">
+                <div class="avatar-holder">
+                    <img src="${articleList[2].imgUrl}" alt="">
+                </div>
+                
+                <h3>${articleList[2].heading}</h3>
+            </div>
+            
+            <p>${articleList[2].text}</p>
+            
+            <a href="${articleList[2].url}">+ Läs hela artiklen</a>
+        </article>
+
+        <article>
+            <div class="d-flex justify-content-start align-items-center">
+                <div class="avatar-holder">
+                    <img src="${articleList[3].imgUrl}" alt="">
+                </div>
+                
+                <h3>${articleList[3].heading}</h3>
+            </div>
+            
+            <p>${articleList[3].text}</p>
+            
+            <a href="${articleList[3].url}">+ Läs hela artiklen</a>
+        </article>
+        
+        `;
+    }
+
 });
