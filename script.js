5793cf5b2c5c70cb71cb464367cb664a52f7192a
Tim Nilsson <tim.r.nilsson@gmail.com>
2021-12-16 18:43:44 +0100

    deployable site

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 const barsEl = document.querySelector('#hamburger');
 const navEl = document.querySelector('#navbar');
-const menuDropEl = document.querySelector('.dropdown-menu-holder');
+const btnCloseMenu = document.querySelector('.btn-close-menu');
 
-const headingEl = document.querySelector('h2');
-const infoEl = document.querySelector('.more-info');
-const btnPrevArticlesEl = document.querySelector('#experience .prev-art');
-const btnNextArticlesEl = document.querySelector('#experience .next-art');
+const infoExplainEl = document.querySelector('.more-info-explain');
+const infoExperienceEl = document.querySelector('.more-info-experience');
+const btnCloseLbExEl = document.querySelector('.close-lb-ex');
+const btnCloseLbExperienceEl = document.querySelector('.close-lb-experience');
 
 barsEl.addEventListener('click', () => {
     navEl.classList.toggle('d-none');
+    barsEl.classList.toggle('d-none');
+    btnCloseMenu.classList.toggle('d-none');
 });
 
-headingEl.addEventListener('click', () => {
-    headingEl.scrollIntoView();
+btnCloseMenu.addEventListener('click', () => {
+    navEl.classList.toggle('d-none');
+    barsEl.classList.toggle('d-none');
+    btnCloseMenu.classList.toggle('d-none');
 });
 
-infoEl.addEventListener('click', (e) => {
+infoExplainEl.addEventListener('click', (e) => {
     e.preventDefault();
     document.querySelector('.lightbox-wrapper').classList.add('show');
 });
 
+infoExperienceEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#experience .lightbox-wrapper').classList.add('show');
+});
 
+btnCloseLbExEl.addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.parentElement.classList.remove('show');
+});
+
+btnCloseLbExperienceEl.addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.parentElement.classList.remove('show');
+});
 
 document.querySelector('.a-services').addEventListener('click', (e) => {
     e.preventDefault();
     document.querySelector('.dropdown-menu-holder2').classList.add('d-none');
     document.querySelector('.dropdown-menu-holder3').classList.add('d-none');
     document.querySelector('.dropdown-menu-holder1').classList.toggle('d-none');
+    document.querySelector('.dropdown-menu-holder1').classList.toggle('animate-m');
+    document.querySelector('.dropdown-menu-holder2').classList.remove('animate-m');
+    document.querySelector('.dropdown-menu-holder3').classList.remove('animate-m');
 });
 
 document.querySelector('.a-knowledge').addEventListener('click', (e) => {
@@ -34,6 +52,9 @@ document.querySelector('.a-knowledge').addEventListener('click', (e) => {
     document.querySelector('.dropdown-menu-holder1').classList.add('d-none');
     document.querySelector('.dropdown-menu-holder3').classList.add('d-none');
     document.querySelector('.dropdown-menu-holder2').classList.toggle('d-none');
+    document.querySelector('.dropdown-menu-holder1').classList.remove('animate-m');
+    document.querySelector('.dropdown-menu-holder2').classList.toggle('animate-m');
+    document.querySelector('.dropdown-menu-holder3').classList.remove('animate-m');
 });
 
 document.querySelector('.a-about').addEventListener('click', (e) => {
@@ -41,6 +62,9 @@ document.querySelector('.a-about').addEventListener('click', (e) => {
     document.querySelector('.dropdown-menu-holder1').classList.add('d-none');
     document.querySelector('.dropdown-menu-holder2').classList.add('d-none');
     document.querySelector('.dropdown-menu-holder3').classList.toggle('d-none');
+    document.querySelector('.dropdown-menu-holder1').classList.remove('animate-m');
+    document.querySelector('.dropdown-menu-holder2').classList.remove('animate-m');
+    document.querySelector('.dropdown-menu-holder3').classList.toggle('animate-m');
 });
 
 const articleList = [
@@ -48,7 +72,7 @@ const articleList = [
         id: 0,
         imgUrl: `img/frank-a100.png`,
         heading: `“Jag vill ju rädda världen”`,
-        text: `Johanna Vesterberg från Luleå brinner för miljön. När hon som, möbel- och textildesigner, ville satsa på något eget var valet enkelt: -Jag mår dåligt av de stora företagens slit och släng. 100 miljoner ton möbler slängs varje år. Nu har vi tagit fram en skandinavisk möbel i helt nytt koncept.`,
+        text: `Johanna Vesterberg från Luleå brinner för miljön. När hon som, möbel- och textildesigner, ville satsa på något eget var valet enkelt: - Jag mår dåligt av de stora företagens slit och släng. 100 miljoner ton möbler slängs varje år. Nu har vi tagit fram en skandinavisk möbel i helt nytt koncept.`,
         url: `https://www.almi.se/kunskapsbanken/inspiration/normada/`
     },
     {
@@ -76,21 +100,8 @@ const articleList = [
 
 let currentArticle = 0;
 const articleOptionEls = document.querySelectorAll('.opt');
-
-document.querySelector('#experience .art-wrapper article').innerHTML = `
-    
-        <div class="d-flex justify-content-start align-items-center">
-            <div class="avatar-holder">
-                <img src="${articleList[0].imgUrl}" alt="">
-            </div>
-            <h3>${articleList[0].heading}</h3>
-        </div>
-
-        <p>${articleList[0].text}</p>
-
-        <a href="${articleList[0].url}">+ Läs hela artiklen</a>
-    
-`;
+const btnPrevArticlesEl = document.querySelector('#experience .prev-art');
+const btnNextArticlesEl = document.querySelector('#experience .next-art');
 
 btnPrevArticlesEl.addEventListener('click', () => {
 
@@ -98,8 +109,10 @@ btnPrevArticlesEl.addEventListener('click', () => {
         articleOptionEls[currentArticle].classList.remove('opt-selected');
         currentArticle--;
         articleOptionEls[currentArticle].classList.add('opt-selected');
-        document.querySelector('#experience .art-wrapper article').innerHTML = `
-        
+
+        document.querySelector('#experience .art-wrapper').innerHTML = `
+
+        <article class="animate">
             <div class="d-flex justify-content-start align-items-center">
                 <div class="avatar-holder">
                     <img src="${articleList[currentArticle].imgUrl}" alt="">
@@ -109,8 +122,9 @@ btnPrevArticlesEl.addEventListener('click', () => {
 
             <p>${articleList[currentArticle].text}</p>
 
-            <a href="${articleList[currentArticle].url}">+ Läs hela artiklen</a>
-        
+            <a href="${articleList[currentArticle].url}" target="_blank">+ Läs hela artiklen</a>
+        </article>
+
         `;
     }
     
@@ -122,8 +136,10 @@ btnNextArticlesEl.addEventListener('click', () => {
         articleOptionEls[currentArticle].classList.remove('opt-selected');
         currentArticle++;
         articleOptionEls[currentArticle].classList.add('opt-selected');
-        document.querySelector('#experience .art-wrapper article').innerHTML = `
+
+        document.querySelector('#experience .art-wrapper').innerHTML = `
         
+        <article class="animate">
             <div class="d-flex justify-content-start align-items-center">
                 <div class="avatar-holder">
                     <img src="${articleList[currentArticle].imgUrl}" alt="">
@@ -133,8 +149,9 @@ btnNextArticlesEl.addEventListener('click', () => {
 
             <p>${articleList[currentArticle].text}</p>
 
-            <a href="${articleList[currentArticle].url}">+ Läs hela artiklen</a>
-        
+            <a href="${articleList[currentArticle].url}" target="_blank">+ Läs hela artiklen</a>
+        </article>
+
         `;
     }
     
@@ -154,7 +171,7 @@ prevArtLg.addEventListener('click', () => {
 
         document.querySelector('#experience .art-wrapper-lg').innerHTML = `
         
-        <article>
+        <article class="animate">
             <div class="d-flex justify-content-start align-items-center">
                 <div class="avatar-holder">
                     <img src="${articleList[0].imgUrl}" alt="">
@@ -165,10 +182,10 @@ prevArtLg.addEventListener('click', () => {
             
             <p>${articleList[0].text}</p>
             
-            <a href="${articleList[0].url}">+ Läs hela artiklen</a>
+            <a href="${articleList[0].url}" target="_blank">+ Läs hela artiklen</a>
         </article>
 
-        <article>
+        <article class="animate">
             <div class="d-flex justify-content-start align-items-center">
                 <div class="avatar-holder">
                     <img src="${articleList[1].imgUrl}" alt="">
@@ -179,7 +196,7 @@ prevArtLg.addEventListener('click', () => {
             
             <p>${articleList[1].text}</p>
             
-            <a href="${articleList[1].url}">+ Läs hela artiklen</a>
+            <a href="${articleList[1].url}" target="_blank">+ Läs hela artiklen</a>
         </article>
         
         `;
@@ -196,7 +213,7 @@ nextArtLg.addEventListener('click', () => {
 
         document.querySelector('#experience .art-wrapper-lg').innerHTML = `
         
-        <article>
+        <article class="animate">
             <div class="d-flex justify-content-start align-items-center">
                 <div class="avatar-holder">
                     <img src="${articleList[2].imgUrl}" alt="">
@@ -207,10 +224,10 @@ nextArtLg.addEventListener('click', () => {
             
             <p>${articleList[2].text}</p>
             
-            <a href="${articleList[2].url}">+ Läs hela artiklen</a>
+            <a href="${articleList[2].url}" target="_blank">+ Läs hela artiklen</a>
         </article>
 
-        <article>
+        <article class="animate">
             <div class="d-flex justify-content-start align-items-center">
                 <div class="avatar-holder">
                     <img src="${articleList[3].imgUrl}" alt="">
@@ -221,7 +238,7 @@ nextArtLg.addEventListener('click', () => {
             
             <p>${articleList[3].text}</p>
             
-            <a href="${articleList[3].url}">+ Läs hela artiklen</a>
+            <a href="${articleList[3].url}" target="_blank">+ Läs hela artiklen</a>
         </article>
         
         `;
